cf9694e9306e22be0c9fb329a4d45a7a3256ca8b
Askhat Salikhov <salihovar@gmail.com>
2018-07-08 18:58:49 +0300

    Initial

--- scripts/DeploymentUtils.groovy
+++ scripts/DeploymentUtils.groovy
@@ -16,8 +16,6 @@ APPLICATION_NAME = 'core-server'
  */
 
 String getEnvironmentByCNAME(String cname) {
-    WsdlProject wsdlProject = new Wsdl
-
     final def jsonSlurper = new JsonSlurper()
 
     final String responseStr = sh("aws elasticbeanstalk describe-environments --application-name ${APPLICATION_NAME}", returnStdout: true)
